f861e9ac3517495ccf91a461eb022e3521c751de
kelum <edirisingheemks@gmail.com>
2018-09-22 00:45:13 +0530

    ssss

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "database" : "mongodb://localhost:27017/hack",
+};
+
+//mongodb://localhost:27017/tidyclean
+//mongodb://tidymaster:tidymaster123@ds133762.mlab.com:33762/tidymaster
